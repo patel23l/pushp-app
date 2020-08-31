07e0e9c5b194dd6227c7dc303fbbe3eaad17ada3
nandinipatel <patelnandini26@gmail.com>
2020-08-31 11:09:36 -0400

    added product detials to redux

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,13 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducer';
+import { productListReducer, productDetailsReducer } from './reducers/productReducer';
 
 const initialState = {};
 const reducer = combineReducers ({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
