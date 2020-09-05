67533b7a5d7cc3dbd5c0477cffbbe94fb1fbd4f8
nandinipatel <patelnandini26@gmail.com>
2020-09-05 16:34:20 -0400

    implemented shopping cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,16 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer, productDetailsReducer } from './reducers/productReducer';
+import Cookie from 'js-cookie';
+import { productListReducer, productDetailsReducer} from './reducers/productReducer';
+import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: {cartItems} };
 const reducer = combineReducers ({
     productList: productListReducer,
     productDetails: productDetailsReducer, 
+    cart: cartReducer,
 })
 
 const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
